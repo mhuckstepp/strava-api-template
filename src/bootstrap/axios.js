fbf5477cf6ea8b886605ad2da8606cd92d05df82
Max Huckstepp <mhuckstepp@strava.com>
2023-11-10 21:41:51 -0800

    Cleanup variables

--- src/bootstrap/axios.js
+++ src/bootstrap/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 import { CONSTANTS } from './constants';
 
-const URL =
-  process.env.NODE_ENV === 'development' ? 'localhost:3010' : CONSTANTS.APP_URL;
-
 export const axiosCodeClient = axios.create({
-  baseURL: `${URL}/users`,
+  baseURL: `${CONSTANTS.APP_URL}/users`,
   method: 'post',
 });
--- src/bootstrap/constants.js
+++ src/bootstrap/constants.js
@@ -1,10 +1,11 @@
 const CLIENT_ID = 'YOUR_APP_CLIENT_ID';
-const APP_URL = 'YOUR_APP_URL';
-const STRAVA_REDIRECT =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : APP_URL;
+const APP_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'YOUR_APP_URL';
 
 export const CONSTANTS = {
-  STRAVA_AUTH_LINK: `http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${STRAVA_REDIRECT}/auth&approval_prompt=auto&scope=activity:read_all,activity:write,profile:read_all`,
+  STRAVA_AUTH_LINK: `http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${APP_URL}/auth&approval_prompt=auto&scope=activity:read_all,activity:write,profile:read_all`,
   STRAVA_ORANGE: '#fc5200',
   APP_URL,
 };
